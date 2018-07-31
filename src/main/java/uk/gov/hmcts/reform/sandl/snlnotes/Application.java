8a1d64b485f88ec4352594b5dc2f0571ca75b165
Remigiusz Kruk <R.Kruk@kainos.com>
2018-07-31 09:13:44 +0200

    initial setup with database

---
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlnotes/Application.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.sandl.snlnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

fe2b62e4613b2704887258a2e0251c4a4f0d7e18
Remigiusz Kruk <R.Kruk@kainos.com>
2018-07-31 09:45:58 +0200

    add tests

---
+++ src/test/functional/java/uk/gov/hmcts/reform/sandl/snlnotes/functional/SmokeTest.java
@@ -0,0 +1,4 @@
+package uk.gov.hmcts.reform.sandl.snlnotes.functional;
+
+public interface SmokeTest { /* category marker */
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlnotes/ApplicationTests.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sandl.snlnotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class ApplicationTests {
+
+    @Test
+    public void sample_test() {
+        assertThat(2).isGreaterThan(1);
+    }
+
+}
